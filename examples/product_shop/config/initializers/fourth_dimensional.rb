78dbf441a65d918b44dc45308a92b54ff3d48348
Baylor Rae <baylor@thecodedeli.com>
2019-02-27 10:04:30 -0600

    reload configuration on change

--- examples/product_shop/config/initializers/fourth_dimensional.rb
+++ examples/product_shop/config/initializers/fourth_dimensional.rb
@@ -1,9 +1,11 @@
-FourthDimensional.configure do |config|
-  config.event_loader = FourthDimensional::EventLoaders::ActiveRecord.new
-  config.command_handlers = [
-    Products::CommandHandler
-  ]
-  config.event_handlers = [
-    Products::ProductProjector
-  ]
+Rails.configuration.to_prepare do
+  FourthDimensional.configure do |config|
+    config.event_loader = FourthDimensional::EventLoaders::ActiveRecord.new
+    config.command_handlers = [
+      Products::CommandHandler
+    ]
+    config.event_handlers = [
+      Products::ProductProjector
+    ]
+  end
 end
