5bfdd267e1aeea5770f21dc9c570b7d5c583b2fd
Baylor Rae <baylor@thecodedeli.com>
2019-02-27 10:03:35 -0600

    reload configuration on file changes

--- lib/generators/fourth_dimensional/templates/initializer.rb
+++ lib/generators/fourth_dimensional/templates/initializer.rb
@@ -1,5 +1,7 @@
-FourthDimensional.configure do |config|
-  config.event_loader = FourthDimensional::EventLoaders::ActiveRecord.new
-  config.command_handlers = []
-  config.event_handlers = []
+Rails.configuration.to_prepare do
+  FourthDimensional.configure do |config|
+    config.event_loader = FourthDimensional::EventLoaders::ActiveRecord.new
+    config.command_handlers = []
+    config.event_handlers = []
+  end
 end
